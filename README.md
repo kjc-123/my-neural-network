d6226cd67e46d433dc285e97c2541efde3940727
Kenneth Chung <pg7408@gmail.com>
2025-11-09 13:54:05 -0800

    reorganized some tests, deleted old files

--- README.md
+++ README.md
@@ -3,11 +3,6 @@ Neural network with one hidden layer training on MNIST
 
 Done out of my personal curiosity
 
-warning - very scuffed; issues include:
- - dimensions of everything are backwards from what is standard
- - only implemented one (1) hidden layer with sixteen (16) neurons; code is not very generalizable in present state
- - many files with useless/broken tests
-
 Special thanks goes to 3blue1brown for his series on Neural Networks, which is how I initially learned everything, and Michael Nielsen, whose neural network implementation I referenced.
 
 3blue1brown's neural network series: https://youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi&si=YxSXqAlSRpeBfm8a
@@ -23,3 +18,4 @@ TODO:
 - [ ] Implement cross-entropy
 - [ ] Implement initialization methods
 - [ ] Generalize problem
+- [ ] Fix unit tests
--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
-# test
+# unit tests
 
-import mynetwork2
+import mynetwork
 import loader
 
 import unittest
@@ -13,27 +13,38 @@ class TestNetworkMethods(unittest.TestCase):
         I1 = np.random.randn(9, 1)
         b1 = np.random.randn(10, 1)
         result = (W1 @ I1) + b1
-        self.assertEqual(mynetwork2.MyNetwork.forwardprop(I1, W1, b1).all(), result.all())
+        self.assertEqual(mynetwork.MyNetwork.forwardprop(I1, W1, b1).all(), result.all())
     def test_cost(self):
         y = np.array([3, 4, 7])
         yhat = np.array([-2, -2, 8])
         result = 62
-        self.assertEqual(mynetwork2.MyNetwork.cost(yhat, y), result)
+        self.assertEqual(mynetwork.MyNetwork.cost(yhat, y), result)
     def test_sigmoid(self):
         z = np.random.randn(16, 1)
         result = 1 / (1 + np.exp(-z))
-        self.assertEqual(mynetwork2.MyNetwork.sigmoid(z).all(), result.all())
-    def test_loader(self):
-        '''result = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
-        ld = loader.loader()
-        data = ld.load_data("data/mnist_test.csv")
-        self.assertEqual(data[0][0], 7)'''
-        self.assertEqual(True, True)
+        self.assertEqual(mynetwork.MyNetwork.sigmoid(z).all(), result.all())
     def test_vectorized_result(self):
         expected = np.array([0., 0., 0., 0., 0., 0., 0., 1., 0., 0.])
         ld = loader.loader()
         observed = ld.vectorized_result(7)
         self.assertEqual(observed.all(), expected.all())
+
+x = 2
+arr1 = np.array([0., 0., 1., 0., 0., 0., 0., 0., 0., 0.])
+arr2 = np.zeros(10)
+arr2[x] = 1
+
+print(arr1)
+print(arr2)
+
+ld = loader.loader()
+data = ld.load_data("data/mnist_test.csv")
+print(data[0][0])
+labels = data[:,0]
+data_size = data.shape[0]
+data[:,0] = np.zeros((data_size, 10))
+print(labels)
+print(data[:,0])
 
 if __name__ == "__main__":
     unittest.main()
